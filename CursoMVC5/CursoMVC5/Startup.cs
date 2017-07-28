cd4b25a1c5214fe32f6e738d0051cc82b664c340
Juan <senxdmc@gmail.com>
2017-07-28 10:51:11 -0400

    Creacion de Proyecto

---
+++ CursoMVC5/CursoMVC5/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CursoMVC5.Startup))]
+namespace CursoMVC5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
