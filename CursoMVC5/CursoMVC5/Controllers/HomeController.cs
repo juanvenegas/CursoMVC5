466cbd2afe446263d7b91e5c24741f9baa5593aa
Juan <senxdmc@gmail.com>
2017-08-01 18:12:08 -0400

    Se Crearon los ejemplos de capitulo 83 sin la libreria de javascript unobtrusive-ajax.js

--- CursoMVC5/CursoMVC5/Controllers/HomeController.cs
+++ CursoMVC5/CursoMVC5/Controllers/HomeController.cs
@@ -26,5 +26,33 @@ namespace CursoMVC5.Controllers
 
             return View();
         }
+
+        [HttpPost]
+        public JsonResult Duplicador(double cantidad)
+        {
+            var duplicado = cantidad * 2;
+            return Json(duplicado);
+        }
+
+        public double DuplicadorComparativo(double cantidad)
+        {
+            var duplicado = cantidad * 2;
+            return duplicado;
+        }
+
+        public ActionResult DuplicadorCantidad_CS(double cantidadCS)
+        {
+            var cantidadDuplicada = DuplicadorComparativo(cantidadCS);
+            ViewBag.Resultado = cantidadDuplicada;
+            return View("Contact");
+        }
+
+        [HttpPost]
+        public ContentResult DuplicadorCantidad_Ajax(double cantidadAjax)
+        {
+            var cantidadDuplicada = DuplicadorComparativo(cantidadAjax);
+            return Content(cantidadDuplicada.ToString());
+            
+        }
     }
 }
